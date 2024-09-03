467d8433e8de962183304ba03ae434b044f0edfd
saviorbp <72775597+saviorbp@users.noreply.github.com>
2024-09-03 12:20:48 -0300

    implemetaçao dos testes

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(test "/home/saviorbp/ed/FinalWork/build/test_executable")
-set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "/home/saviorbp/ed/FinalWork/CMakeLists.txt;11;add_test;/home/saviorbp/ed/FinalWork/CMakeLists.txt;0;")
+add_test(data_processor "/home/saviorbp/ed/FinalWork/build/test_executable")
+set_tests_properties(data_processor PROPERTIES  _BACKTRACE_TRIPLES "/home/saviorbp/ed/FinalWork/CMakeLists.txt;11;add_test;/home/saviorbp/ed/FinalWork/CMakeLists.txt;0;")
--- src/test.cpp
+++ src/test.cpp
@@ -1,3 +1,38 @@
-//testes unitarios
+#include "dataProcessor.h"
+#include <iostream>
+#include <filesystem>
 
-//testes unitarios
+int main()
+{
+    std::string csvFilePath = "/home/saviorbp/ed/FinalWork/data/movies_dataset.csv";
+    std::filesystem::path directoryPath = std::filesystem::path(csvFilePath).parent_path();
+    std::string binFilePath = (directoryPath / "movies_dataset.bin").string();
+
+    // Verificar se o diretório e o arquivo CSV existem
+    if (!std::filesystem::exists(csvFilePath) || !std::filesystem::exists(csvFilePath))
+    {
+        std::cerr << "Diretório ou arquivo CSV não encontrado!" << std::endl;
+        return 1;
+    }
+
+    dataProcessor processor;
+
+    // Convertendo CSV para binário
+    processor.convertCsvToBinary(csvFilePath, binFilePath);
+    std::cout << "CSV convertido para binário com sucesso!" << std::endl;
+
+    // Extraindo todos os filmes
+    auto movies = processor.extractAllMovies(binFilePath);
+    std::cout << "Total de filmes extraídos: " << movies.size() << std::endl;
+
+    // Extraindo N filmes aleatórios
+    int n = 10; // Exemplo: extraindo 10 filmes
+    auto randomMovies = processor.extractRandomMovies(binFilePath, n);
+    std::cout << "Filmes aleatórios extraídos:" << std::endl;
+    for (const auto &movie : randomMovies)
+    {
+        movie.print();
+    }
+
+    return 0;
+}
